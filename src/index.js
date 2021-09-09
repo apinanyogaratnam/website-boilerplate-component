039289a21fbcc4cf5fbddc26afea4705915e7249
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-08 20:01:02 -0400

    created MainPage component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+    return <div className={styles.test}>Example Component: {text}</div>
+}
+
+export const MainPage = () => {
+    return <div>Hello World</div>
 }
