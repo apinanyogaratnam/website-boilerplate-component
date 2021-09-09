fde1bdad4912401096dfb3f951165fee3d3e0ec1
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-08 20:37:31 -0400

    passed in example props of my socials

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,14 @@ import 'website-boilerplate-component/dist/index.css'
 const App = () => {
   return (
       <div>
-          <MainPage name="Apinan Yogaratnam" description="A motivated programmer" />
+          <MainPage 
+              name="Apinan Yogaratnam" 
+              description="A motivated programmer"
+              linkedin="https://www.linkedin.com/in/apinan-yogaratnam"
+              github="https://github.com/apinanyogaratnam"
+              email="apinanapinan@icloud.com"
+              facebook="https://www.facebook.com/apinan.yogaratnam.7" 
+          />
       </div>
   );
 }
