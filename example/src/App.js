a0450d63d4f8852f759f8d82102e0be597679160
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-08 20:04:37 -0400

    calling mainpage component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'website-boilerplate-component'
+import { ExampleComponent, MainPage } from 'website-boilerplate-component'
 import 'website-boilerplate-component/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+      <div>
+        <ExampleComponent text="Create React Library Example 😄" />
+        <MainPage />
+      </div>
+  );
 }
 
 export default App
