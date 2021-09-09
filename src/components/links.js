a8d5ba4bd912487d5439f932f5e76a93db135303
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-08 20:41:08 -0400

    put links into h4 tags

--- src/components/links.js
+++ src/components/links.js
@@ -4,10 +4,10 @@ export const Links = ({ linkedin = undefined, facebook = undefined, github = und
     return (
         <div>
             <h2>Links</h2>
-            {linkedin ? <a href={linkedin} target="_blank" rel="noopener noreferrer">LinkedIn</a> : null}
-            {facebook ? <a href={facebook} target="_blank" rel="noopener noreferrer">Facebook</a> : null}
-            {github ? <a href={github} target="_blank" rel="noopener noreferrer">Github</a> : null}
-            {email ? <a href={`mailto:${email}`}>Email</a> : null}
+            <h4>{linkedin ? <a href={linkedin} target="_blank" rel="noopener noreferrer">LinkedIn</a> : null}</h4>
+            <h4>{facebook ? <a href={facebook} target="_blank" rel="noopener noreferrer">Facebook</a> : null}</h4>
+            <h4>{github ? <a href={github} target="_blank" rel="noopener noreferrer">Github</a> : null}</h4>
+            <h4>{email ? <a href={`mailto:${email}`}>Email</a> : null}</h4>
         </div>
     )
 }
