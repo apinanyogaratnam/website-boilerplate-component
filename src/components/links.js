1cf95658f396328a10e3a31862f6635e2f16e8db
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-08 20:32:17 -0400

    defined prop arguments for links component

--- src/components/links.js
+++ src/components/links.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Links = () => {
+export const Links = ({ linkedin = undefined, facebook = undefined, github = undefined, email = undefined }) => {
     return (
         <div>
             <h2>Links</h2>
