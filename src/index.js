46a1d1319914d281dd3312ce88fc27894f6760f9
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-08 20:38:40 -0400

    added more prop args and passed in more prop args to load on page

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import styles from './styles.module.css';
 import { Links } from './components/links.js';
 
 
-export const MainPage = ({ name, description }) => {
+export const MainPage = ({ name, description, linkedin, github, facebook, email }) => {
     return (
         <div className="center-text">
             <div className="title">{name}</div>
             <br/><br/>
             <h2>About Me</h2>
             <p>{description}</p>
-            <Links />
+            <Links linkedin={linkedin} github={github} facebook={facebook} email={email} />
         </div>
     );
 }
