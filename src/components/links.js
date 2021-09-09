a4b619d22336294e6181fab6ff5ca14206c35bf6
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-08 20:38:58 -0400

    added social links with error checking to not load undefined links

--- src/components/links.js
+++ src/components/links.js
@@ -4,6 +4,10 @@ export const Links = ({ linkedin = undefined, facebook = undefined, github = und
     return (
         <div>
             <h2>Links</h2>
+            {linkedin ? <a href={linkedin} target="_blank" rel="noopener noreferrer">LinkedIn</a> : null}
+            {facebook ? <a href={facebook} target="_blank" rel="noopener noreferrer">Facebook</a> : null}
+            {github ? <a href={github} target="_blank" rel="noopener noreferrer">Github</a> : null}
+            {email ? <a href={`mailto:${email}`}>Email</a> : null}
         </div>
     )
 }
